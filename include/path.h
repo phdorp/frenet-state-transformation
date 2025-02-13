75782a7910edd02497ea3d515ac5f7a192ef6dea
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-02-13 19:59:56 +0100

    docs: describe path responsibilities

---
+++ include/path.h
@@ -0,0 +1,11 @@
+
+/**
+ * @brief Path representation as polyline
+ *
+ * Represents a 2-dimensional path as a polyline.
+ * Provide path properties based on finite differences at query points.
+ * The properties include orientation, curvature, cuvature change, normal and tangential.
+ */
+class Path
+{
+};
