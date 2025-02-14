e3f13dedb8162e74c948ddb72a721b425bffcff2
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-02-14 20:46:24 +0100

    docs: describe transform responsibilities

---
+++ include/transform.h
@@ -0,0 +1,17 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+namespace FrenetTransform
+{
+    /**
+     * @brief Transformation between Cartesian and Frenet frame
+     *
+     * Use the path properties to implement the transformation independent from the path implementation.
+     */
+    class Transform
+    {
+
+    };
+};
+
+#endif
