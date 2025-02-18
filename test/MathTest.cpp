66f49b1adffe18ca2bb19d2021ac158451bc5275
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-02-18 16:40:09 +0100

    test: function diff with deterministic matrix and vector

--- test/MathTest.cpp
+++ test/MathTest.cpp
@@ -3,22 +3,52 @@
 #include <eigen3/Eigen/Core>
 #include <gtest/gtest.h>
 
-
-TEST(diff, RandomVector)
+/**
+ * @brief Test function diff with vector of length 5.
+ *
+ */
+TEST(diff, Vector5)
 {
-    Eigen::Array<double, 5, 1> vector {Eigen::Array<double, 5, 1>::Random()};
-    auto result {FrenetTransform::diff(vector)};
-    EXPECT_EQ(result.rows(), vector.rows());
+    Eigen::Array<double, 5, 1> input {1, 2, 5, 1, 3}; // input vector
 
-    EXPECT_NEAR(result(0), 0.0, 0.000001);
+    auto result {FrenetTransform::diff(input)}; // function result
 
-    for(int cNumber {1}; cNumber < result.rows(); ++cNumber)
-    {
-        EXPECT_DOUBLE_EQ(result(cNumber), vector(cNumber) - vector(cNumber-1));
-        ++cNumber;
-    }
+    EXPECT_EQ(result.rows(), input.rows()); // test equality of input and result rows
+
+    Eigen::Array<double, 5, 1> groundTruth {0, 1, 3, -4, 2}; // ground truth result
+
+    // test for equality between ground truth and result
+    for(int index {}; index < result.rows(); ++index)
+        EXPECT_NEAR(result(index), groundTruth(index), 1.0e-10);
 }
 
+TEST(diff, Matrix52)
+{
+    // input matrix
+    Eigen::Array<double, 5, 2> input {};
+    input.col(0) << 1, 2, 5, 1, 3;
+    input.col(1) << 2, 2, 5, 1, 3;
+
+    auto result {FrenetTransform::diff(input)}; // function result
+
+    EXPECT_EQ(result.rows(), input.rows()); // test equality of input and result rows
+    EXPECT_EQ(result.cols(), input.cols()); // test equality of input and result columns
+
+    // ground truth result
+    Eigen::Array<double, 5, 2> groundTruth {};
+    groundTruth.col(0) << 0, 1, 3, -4, 2;
+    groundTruth.col(1) << 0, 0, 3, -4, 2;
+
+    // test for equality between ground truth and result
+    for(int row {}; row < result.rows(); ++row)
+        for(int col {}; col < result.cols(); ++col)
+            EXPECT_NEAR(result(row, col), groundTruth(row, col), 1.0e-10);
+}
+
+/**
+ * @brief Test partial length with straigth line.
+ *
+ */
 TEST(partialLength, StraightLine)
 {
     Eigen::Array<double, 5, 1> x {0.0, 1.0, 2.0, 3.0, 4.0};
