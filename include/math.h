524b0c17b43b968c86d16510e16b382c6f3e75f2
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-02-18 16:13:04 +0100

    doc: provide docstrings for functions diff, partialLength

--- include/math.h
+++ include/math.h
@@ -8,6 +8,15 @@ namespace FrenetTransform
     template <int T>
     using ArrayT1 = Eigen::Array<double, T, 1>;
 
+    /**
+     * @brief Perform row-wise backward differences on the input array.
+     *
+     * Due to backward differences, the first row of the return array is zero.
+     *
+     * @tparam T array type.
+     * @param numbers input array.
+     * @return T array of row-wise differences.
+     */
     template <typename T>
     T diff(const Eigen::ArrayBase<T>& numbers)
     {
@@ -16,6 +25,14 @@ namespace FrenetTransform
         return result;
     }
 
+    /**
+     * @brief Provide cumulative lengths input given points.
+     *
+     * @tparam T row of input vector.
+     * @param x coordinates in x-direction.
+     * @param y coordinates in y-direction.
+     * @return ArrayT1<T> cumulative lengths input given points.
+     */
     template <int T>
     ArrayT1<T> partialLength(const ArrayT1<T>& x, const ArrayT1<T>& y)
     {
