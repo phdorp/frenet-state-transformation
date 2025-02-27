852948f3894b23a9fbb0d42bae3ec7fccc8feafd
CoatGoal <philip.dorpmueller@web.de>
2025-02-27 19:40:26 +0100

    tests: add test for position transform to frenet

--- test/PolylineTest.cpp
+++ test/PolylineTest.cpp
@@ -1,9 +1,11 @@
 #include "polyline.h"
 #include "points.h"
+#include "transform.h"
 
 #include <gtest/gtest.h>
 #include <eigen3/Eigen/Core>
 #include <math.h>
+#include <memory>
 
 using FrenetTransform::Points;
 
@@ -14,6 +16,7 @@ class PolylineTest : public testing::Test
     PolylineTest()
         : m_straight { {0, 1, 2, 4, 7},  {0, 1, 2, 4, 7} }
         , m_circle { m_radius * m_lengthsCircle.cos(), m_radius * m_lengthsCircle.sin() }
+        , m_circleTransform { std::make_shared<FrenetTransform::Polyline<1079>>(m_circle) }
     {
     }
 
@@ -22,6 +25,7 @@ class PolylineTest : public testing::Test
     const double m_radius { 10.0 };
     const Eigen::Array<double, 1079, 1> m_lengthsCircle { Eigen::Array<double, 1079, 1>::LinSpaced(-M_PI, M_PI)} ;
     const FrenetTransform::Polyline<1079> m_circle {};
+    const FrenetTransform::Transform m_circleTransform;
 };
 
 TEST_F(PolylineTest, GetPointsStraight)
@@ -185,6 +189,27 @@ TEST_F(PolylineTest, NextPointsCircle)
     {
         EXPECT_NEAR(groundTruth.x()(index), result.x()(index), 1e-2);
         EXPECT_NEAR(groundTruth.y()(index), result.y()(index), 1e-2);
+    }
+}
+
+TEST_F(PolylineTest, PosFrenetCircle)
+{
+    const Points<Eigen::Dynamic> input {
+        {{      0.0,  0.0, 1.0}},
+        {{-m_radius, -2.0, 1.0}}
+    };
+
+    const auto result { m_circleTransform.posFrenet(input) };
+
+    const Points<Eigen::Dynamic> groundTruth {
+        {{ M_PI / 2 * m_radius, M_PI / 2 * m_radius, 5 * M_PI / 4 * m_radius }},
+        {{                 0.0,      m_radius - 2.0, m_radius - std::sqrt(2) }}
+    };
+
+    for(int index {}; index < input.numPoints(); ++index)
+    {
+        EXPECT_NEAR(groundTruth.x(index), result.x(index), 1e-2);
+        EXPECT_NEAR(groundTruth.y(index), result.y(index), 1e-2);
     }
 }
 
