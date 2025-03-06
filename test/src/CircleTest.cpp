92bdee563c77b903a75cc4d97ef7dca5f5ead7cd
Philip Dorpmüller <philip.dorpmueller@tu-dortmund.de>
2025-03-06 07:35:48 +0100

    refactor: compact angle test

--- test/src/CircleTest.cpp
+++ test/src/CircleTest.cpp
@@ -6,21 +6,21 @@ namespace FrenetTransform
     {
         TEST_F(CircleTest, asdf)
         {
-            const auto result { m_circle.angle0(m_posFrenet.x()) };
+            const auto anglesEst { m_circle.angle0(m_posFrenet.x()) };
 
-            Eigen::ArrayXd groundTruth { m_posCircle.y() + M_PI / 2};
-            for(int index {}; index < groundTruth.rows(); ++index)
-                if(groundTruth(index) > M_PI)
-                    groundTruth(index) += -2*M_PI;
+            Eigen::ArrayXd anglesGtr { m_posCircle.y() + M_PI / 2};
+            // map angle in -PI to PI
+            std::for_each(anglesGtr.begin(), anglesGtr.end(),
+                [](double& angle) { angle = std::fmod(angle + M_PI, 2 * M_PI) - M_PI; });
 
-            for(int index {}; index < groundTruth.rows(); ++index)
-                EXPECT_NEAR(groundTruth(index), result(index), 1e-2);
+            expectAllClose(anglesEst, anglesGtr, 1e-2);
         }
     };
 };
 
 int main(int argc, char **argv)
 {
+    std::srand(0);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
