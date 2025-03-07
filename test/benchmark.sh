1b139f3d7d90752eeeb1b0fd3368c609362b8cc1
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-03-07 16:10:39 +0100

    feat: add benchmarking script

---
+++ test/benchmark.sh
@@ -0,0 +1,2 @@
+mkdir -p ../out
+./build/test/src/benchmarks/circleBenchmark --benchmark_out=../out/benchmark.json --benchmark_out_format=json --benchmark_counters_tabular=true --benchmark_time_unit=ms
