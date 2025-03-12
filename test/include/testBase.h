8d4afae1db3916cb2228ed6345a6bbdcc4046997
CoatGoal <philip.dorpmueller@web.de>
2025-03-12 15:32:35 +0100

    feat: provide index where tolerance not apolies

--- test/include/testBase.h
+++ test/include/testBase.h
@@ -17,7 +17,13 @@ namespace FrenetTransform
             void expectAllClose(const Eigen::ArrayBase<Tarray>& estimate, const Eigen::ArrayBase<Tarray>& groundTruth, double errAbs = limits::infinity(), double errRel = limits::infinity())
             {
                 const auto errAbsVals { (estimate - groundTruth).abs() };
-                EXPECT_LT(*std::max_element(errAbsVals.begin(), errAbsVals.end()), errAbs);
+                const auto errRelVals { errAbsVals / groundTruth };
+
+                for(int idx {}; idx < estimate.size(); ++idx)
+                {
+                    EXPECT_LT(errAbsVals(idx), errAbs) << "Error bound violated at index " << maxIdx << '!';
+                    EXPECT_LT(errRelVals(idx), errRel) << "Error bound violated at index " << maxIdx << '!';
+                }
             }
         };
     };
