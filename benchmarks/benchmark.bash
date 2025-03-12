f396d24871cdde36e0962f942ec47f3e5fb50d7a
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-03-12 20:46:15 +0100

    feat: enable benchmark of commits

--- benchmarks/benchmark.bash
+++ benchmarks/benchmark.bash
@@ -1,3 +1,12 @@
 #!/bin/bash
 mkdir -p benchmarks/logs
-$BENCHMAKR_BINS/circleBenchmark --benchmark_out=$WORKING_DIRECTORY/benchmarks/logs/benchmark.json --benchmark_out_format=json --benchmark_counters_tabular=true --benchmark_time_unit=ms
+for commit in $*;
+do
+    git checkout $commit
+    rm -r build <&-
+    mkdir build
+    cmake -B build -D BUILD_BENCHMARK=ON -D CMAKE_CXX_COMPILER=g++-13
+    cmake --build build
+    DATETIME=$(git show -s --format=%cd --date=format:'%y%m%d-%H%M' $commit)
+    $BENCHMAKR_BINS/circleBenchmark --benchmark_out=$WORKING_DIRECTORY/benchmarks/logs/$DATETIME-$commit.json --benchmark_out_format=json --benchmark_counters_tabular=true --benchmark_time_unit=ms --benchmark_context=commit=$commit
+done
