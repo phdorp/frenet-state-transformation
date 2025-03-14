574b387a22f9124769af27a4d99a7f20c8b4875e
CoatGoal <philip.dorpmueller@web.de>
2025-03-14 16:01:03 +0100

    refactor: add descriptive template paramter names

--- include/frenetTransform/math.h
+++ include/frenetTransform/math.h
@@ -5,26 +5,26 @@
 
 namespace FrenetTransform
 {
-    template <int T>
-    using ArrayT1 = Eigen::Array<double, T, 1>;
+    template <int NumRows>
+    using ArrayRows = Eigen::Array<double, NumRows, 1>;
 
     /**
      * @brief Perform row-wise backward differences on the input array.
      *
      * Due to backward differences, the first row of the return array is zero.
      *
-     * @tparam T array type.
+     * @tparam ArrayType array type.
      * @param numbers input array.
-     * @return T array of row-wise differences.
+     * @return ArrayType array of row-wise differences.
      */
-    template <typename T>
-    T diffBackward(const Eigen::ArrayBase<T>& numbers)
+    template <typename ArrayType>
+    ArrayType diffBackward(const Eigen::ArrayBase<ArrayType>& numbers)
     {
         const auto colsAll {Eigen::seq(0, numbers.cols() - 1)}; // sequence over all column indices
         const auto rowsExLast {Eigen::seq(0, numbers.rows()-2)}; // sequence over all row indices except last one
         const auto rowsExFirst {Eigen::seq(1, numbers.rows()-1)}; // sequence over all row indices except first one
 
-        T result { Eigen::ArrayBase<T>::Zero(numbers.rows(), numbers.cols()) }; // result array
+        ArrayType result { Eigen::ArrayBase<ArrayType>::Zero(numbers.rows(), numbers.cols()) }; // result array
 
         // perform differences
         result(rowsExFirst, colsAll) = numbers(rowsExFirst, colsAll) - numbers(rowsExLast, colsAll);
@@ -39,14 +39,14 @@ namespace FrenetTransform
      *
      * Due to forward differences, the first row of the return array is zero.
      *
-     * @tparam T array type.
+     * @tparam ArrayType array type.
      * @param numbers input array.
-     * @return T array of row-wise differences.
+     * @return ArrayType array of row-wise differences.
      */
-    template <typename T>
-    T diffForward(const Eigen::ArrayBase<T>& numbers)
+    template <typename ArrayType>
+    ArrayType diffForward(const Eigen::ArrayBase<ArrayType>& numbers)
     {
-        T result { diffBackward(numbers) }; // get backward differences
+        ArrayType result { diffBackward(numbers) }; // get backward differences
 
         const auto colsAll {Eigen::seq(0, numbers.cols() - 1)}; // sequence over all column indices
         const auto rowsExLast {Eigen::seq(0, numbers.rows() - 2)}; // sequence over all row indices except last one
@@ -62,15 +62,15 @@ namespace FrenetTransform
     /**
      * @brief Provide cumulative lengths input given points.
      *
-     * @tparam T row of input vector.
+     * @tparam NumPoints row of input vector.
      * @param x coordinates in x-direction.
      * @param y coordinates in y-direction.
-     * @return ArrayT1<T> cumulative lengths input given points.
+     * @return ArrayRows<NumPoints> cumulative lengths input given points.
      */
-    template <int T>
-    ArrayT1<T> partialLength(const ArrayT1<T>& x, const ArrayT1<T>& y)
+    template <int NumPoints>
+    ArrayRows<NumPoints> partialLength(const ArrayRows<NumPoints>& x, const ArrayRows<NumPoints>& y)
     {
-        ArrayT1<T> result {FrenetTransform::diffBackward(x).pow(2) + FrenetTransform::diffBackward(y).pow(2)}; // sum of squared distances
+        ArrayRows<NumPoints> result {FrenetTransform::diffBackward(x).pow(2) + FrenetTransform::diffBackward(y).pow(2)}; // sum of squared distances
         result = result.sqrt(); // determine root of sum of squared distances
 
         // accumulate distances
@@ -83,12 +83,12 @@ namespace FrenetTransform
     /**
      * @brief Provide index of minimum positive element in an ordered sequence.
      *
-     * @tparam T rows of input vector.
+     * @tparam ArrayType rows of input vector.
      * @param sequence input to search.
      * @return int index of minimum positive element. -1 if no positive element.
      */
-    template <typename T>
-    int first(const Eigen::ArrayBase<T>& sequence)
+    template <typename ArrayType>
+    int first(const Eigen::ArrayBase<ArrayType>& sequence)
     {
         int index {};
 
@@ -105,37 +105,37 @@ namespace FrenetTransform
     /**
      * @brief Determine gradient from finite differences.
      *
-     * @tparam rows number of samples.
-     * @tparam cols number of dependent variables.
+     * @tparam NumRows number of samples.
+     * @tparam NumCols number of dependent variables.
      * @param depents dependent variables.
      * @param indepents independent variables.
-     * @return Eigen::Array<double, rows, cols> finite difference gradient.
+     * @return Eigen::Array<double, NumRows, NumCols> finite difference gradient.
      */
-    template <int rows, int cols>
-    Eigen::Array<double, rows, cols> gradient(const Eigen::Array<double, rows, cols>& depents, const Eigen::Array<double, rows, 1>& indepents)
+    template <int NumRows, int NumCols>
+    Eigen::Array<double, NumRows, NumCols> gradient(const Eigen::Array<double, NumRows, NumCols>& depents, const Eigen::Array<double, NumRows, 1>& indepents)
     {
-        const Eigen::Array<double, rows, cols> diffDepents { diffBackward(depents) }; // finite differences dependent variables
-        const Eigen::Array<double, rows, 1> diffIndepents { diffBackward(indepents) }; // finite differences independent variables
+        const Eigen::Array<double, NumRows, NumCols> diffDepents { diffBackward(depents) }; // finite differences dependent variables
+        const Eigen::Array<double, NumRows, 1> diffIndepents { diffBackward(indepents) }; // finite differences independent variables
 
-        Eigen::Array<double, rows, cols> result { Eigen::Array<double, rows, cols>::Zero(depents.rows(), depents.cols()) }; // instantiate result array
+        Eigen::Array<double, NumRows, NumCols> result { Eigen::Array<double, NumRows, NumCols>::Zero(depents.NumRows(), depents.NumCols()) }; // instantiate result array
 
         // perform column-wise normalization of dependent differences
-        for(int col {}; col < cols; ++col)
+        for(int col {}; col < NumCols; ++col)
             result(Eigen::all, col) = diffDepents(Eigen::all, col) / diffIndepents;
 
         // results from 0 / 0 are set to 0
-        for(int row {}; row < rows; ++row)
+        for(int row {}; row < NumRows; ++row)
             if(std::abs(diffIndepents(row)) < 1e-10)
-                for(int col {}; col < cols; ++col)
+                for(int col {}; col < NumCols; ++col)
                     result(row, col) = std::abs(diffDepents(row, col)) < 1e-10 ? 0 : result(row, col);
 
         return result;
     }
 
-    template <typename Tarray>
-    Tarray angleDir(const Eigen::ArrayBase<Tarray>& dirxs, const Eigen::ArrayBase<Tarray>& dirys)
+    template <typename ArrayType>
+    ArrayType angleDir(const Eigen::ArrayBase<ArrayType>& dirxs, const Eigen::ArrayBase<ArrayType>& dirys)
     {
-        Tarray angles { Eigen::ArrayBase<Tarray>::Zero(dirxs.rows(), dirxs.cols()) };
+        ArrayType angles { Eigen::ArrayBase<ArrayType>::Zero(dirxs.rows(), dirxs.cols()) };
 
         for(int row {}; row < dirxs.rows(); ++row)
         {
