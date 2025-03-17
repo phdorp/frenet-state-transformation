96ef38d9595cf0566022162782c0e67038a882b7
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-03-17 20:49:16 +0100

    chore: correct typo

--- benchmarks/benchmark.bash
+++ benchmarks/benchmark.bash
@@ -8,5 +8,5 @@ do
     cmake -B build -D BUILD_BENCHMARK=ON -D CMAKE_CXX_COMPILER=g++-13
     cmake --build build
     DATETIME=$(git show -s --format=%cd --date=format:'%y%m%d-%H%M' $commit)
-    $BENCHMAKR_BINS/circleBenchmark --benchmark_out=$WORKING_DIRECTORY/benchmarks/logs/$DATETIME-$commit.json --benchmark_out_format=json --benchmark_counters_tabular=true --benchmark_time_unit=ms --benchmark_context=commit=$commit
+    $BENCHMARK_BINS/circleBenchmark --benchmark_out=$WORKING_DIRECTORY/benchmarks/logs/$DATETIME-$commit.json --benchmark_out_format=json --benchmark_counters_tabular=true --benchmark_time_unit=ms --benchmark_context=commit=$commit
 done
