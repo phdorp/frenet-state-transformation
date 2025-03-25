2757f9eaeea8a77eaf661d1033a4e5bd16e3f0e7
phdorp <philip.dorpmueller@web.de>
2025-03-25 19:35:54 +0100

    docs: add docstrings to path circle test

--- include/frenetTransform/test/pathCircleTest.h
+++ include/frenetTransform/test/pathCircleTest.h
@@ -10,15 +10,21 @@
 
 namespace FrenetTransform {
 namespace Internal {
+/**
+ * @brief Class for testing polyline and transformation functions against circle
+ * as ground truth.
+ *
+ * @tparam NumQueries number of query points to transform, -1 is dynamic.
+ */
 template <typename NumQueries> class PathCircleTest : public TestBase {
 public:
   static constexpr int s_numQueries{
-      NumQueries::s_val == -1 ? 100 : NumQueries::s_val};
+      NumQueries::s_val == -1 ? 100 : NumQueries::s_val}; /**<< number of query points for point generation */
 
 protected:
-  const Circle<NumQueries::s_val> m_circle{5.0, {0.0, 0.0}, -M_PI};
+  const Circle<NumQueries::s_val> m_circle{5.0, {0.0, 0.0}, -M_PI}; /**<< Circle of radius 5, starting at -PI */
   const TransformCircle<NumQueries::s_val> m_transform{
-      std::make_shared<Circle<NumQueries::s_val>>(m_circle)};
+      std::make_shared<Circle<NumQueries::s_val>>(m_circle)}; /**<< Circle Transform as transformation ground truth */
 
   const Points<NumQueries::s_val, PointCircle> m_posCircle{
       m_circle.radius() *
@@ -26,9 +32,9 @@ protected:
            Eigen::Array<double, NumQueries::s_val, 1>::Random(s_numQueries) *
                0.95),
       Eigen::Array<double, NumQueries::s_val, 1>::Random(s_numQueries) * M_PI *
-          0.95};
+          0.95}; /**<< "s_numQueries" number of points in Circle frame */
   const Points<NumQueries::s_val> m_posFrenet{
-      m_transform.posFrenet(m_posCircle)};
+      m_transform.posFrenet(m_posCircle)}; /**<< "m_posCircle" in Frenet frame */
 };
 }; // namespace Internal
 }; // namespace FrenetTransform
