58a878d3975d3fa343d82ba952a8ec55c02aad62
phdorp <philip.dorpmueller@web.de>
2025-03-25 19:35:33 +0100

    docs: add docstrings to polyline line test

--- include/frenetTransform/test/pathPolylineLineTest.h
+++ include/frenetTransform/test/pathPolylineLineTest.h
@@ -11,15 +11,23 @@
 
 namespace FrenetTransform {
 namespace Internal {
+/**
+ * @brief Class for testing polyline and transformation functions against line
+ * as ground truth.
+ *
+ */
 class PathPolylineLineTest : public TestBase {
 protected:
-  const Line<Eigen::Dynamic> m_line{{0.0, 0.0}, {1.0, 2.0}};
+  const Line<Eigen::Dynamic> m_line{
+      {0.0, 0.0}, {1.0, 2.0}}; /**<< Line between (0, 0) and (1, 2) */
 
   const Points<Eigen::Dynamic> m_pointsCartes{
-      Eigen::ArrayXd::Random(100).abs(), Eigen::ArrayXd::Random(100).abs()};
+      Eigen::ArrayXd::Random(100).abs(),
+      Eigen::ArrayXd::Random(100)
+          .abs()}; /**<< 100 random points between in unit square */
 
-  const Polychain<Eigen::Dynamic> m_polyline{
-      m_line(Eigen::ArrayXd{{0.0, 1.0, 1.5, 2.23}})};
+  const Polychain<Eigen::Dynamic> m_polyline{m_line(
+      Eigen::ArrayXd{{0.0, 1.0, 1.5, 2.23}})}; /**<< Polychain along the Line */
 };
 }; // namespace Internal
 }; // namespace FrenetTransform
