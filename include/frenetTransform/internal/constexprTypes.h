85341f16cf10492616fbf65084c8f00837cd7ce3
phdorp <philip.dorpmueller@web.de>
2025-03-25 17:45:17 +0100

    docs: add docstrings for constexpr types

--- include/frenetTransform/internal/constexprTypes.h
+++ include/frenetTransform/internal/constexprTypes.h
@@ -5,8 +5,21 @@
 
 namespace FrenetTransform {
 namespace Internal {
+/**
+ * @brief Struct containing an integral type for the definition of test
+ * parameters.
+ *
+ * @tparam Value integral value.
+ */
 template <int Value> struct Integral { static constexpr int s_val{Value}; };
 
+/**
+ * @brief Struct containing an array for for the definition of test parameters.
+ *
+ * @tparam ValType array element type.
+ * @tparam NumVals number of array elements.
+ * @tparam Vals array elements.
+ */
 template <typename ValType, int NumVals, std::array<ValType, NumVals> Vals>
 struct ConstVals {
   static constexpr std::array<ValType, NumVals> s_vals{Vals};
