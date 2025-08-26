5910f615fa1741a09436c5e0c556ed97045e1f1a
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-08-26 08:40:17 +0200

    build: pystubs require reinstall
    Installation of pystubs now requires installing the package twice.
    The decision is made in favor of automated whl generation.

--- bindings/python/frenetTransform/build_stubs.py
+++ bindings/python/frenetTransform/build_stubs.py
@@ -1,25 +1,14 @@
 import os
-import shutil
-import sys
 
 import pybind11_stubgen as stubgen
-from frenetTransform import _core as transform
 
-# stub out directory defaults to build if not provided
-pathBuild = "build"
-if len(sys.argv) == 2:
-    pathBuild = sys.argv[1]
-pathPybinds = pathBuild + "/bindings/python"
+# import enables process by stubgen
+from frenetTransform import _core
 
 # generate stubs after building the package
-stubgen.main(["-o", pathPybinds, "--root-suffix", "Stubs", "frenetTransform"])
-
-# copy stubs to the package directory and preserve files in build
-shutil.copytree(
-    pathPybinds + "/frenetTransformStubs",
-    os.path.dirname(transform.__file__),
-    dirs_exist_ok=True,
+stubgen.main(
+    ["-o", os.environ["PATH_PYBINDS"], "--root-suffix", "Stubs", "frenetTransform"]
 )
 
 # mypy does not recognize .pyi files without a py.typed marker file
-open(os.path.dirname(transform.__file__) + "/py.typed", "w", encoding="utf-8")
+open(os.environ["PATH_PYSTUBS"] + "/py.typed", "w", encoding="utf-8")
